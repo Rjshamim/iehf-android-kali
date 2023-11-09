94420f33546c939922722ddde52fd93e6a09a84a
Rjshamim <127949166+Rjshamim@users.noreply.github.com>
2023-11-09 22:21:28 +0600

    Add files via upload
    bash iehf.sh

---
+++ iehf.sh
@@ -0,0 +1,33 @@
+pkg update -y && pkg upgrade -y
+termux-setup-storage -y
+pkg install wget -y
+wget -O iehf https://offs.ec/2MceZWr
+chmod 777 iehf
+./iehf
+nh -r
+apt update -y && apt upgrade -y
+apt install dbus-x11 -y
+exit
+nh kex
+iehf909
+iehf909
+n
+https://store.nethunter.com/en/
+nano /etc/resolv.conf
+8.8.8.8 -y
+apt-get update -y && apt-get upgrade -y
+apt-get install git -y
+apt-get install python -y && apt-get install py>
+apt-get install hydra -y
+apt-get install figlet -y
+apt-get dist-upgrade
+figlet Tools is Installed
+python2 IEHF.py
+echo How to use  IEHF Islamic Force
+echo Command : python2 IEHF.py
+echo  fix server Command :nano /etc/resolv.conf
+echo server dns 8.8.8.8
+echo call help 01701028891
+echo visit my wedsite :https://rjshamim.github.io/IF/
+echo kex vnc link https://store.nethunter.com/en/
+echo suscribe our youtube : https://youtube.com/@iehfislamicforce?si=18RhNDlTWJ2hr7b5
